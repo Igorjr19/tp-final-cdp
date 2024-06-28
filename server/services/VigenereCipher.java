fe7935321eae35ea3cdef4516b3038279545a748
Igor Rodrigues <igorjr2003@gmail.com>
2024-06-27 21:44:42 -0300

    ♻️ Refactor cipher service

--- server/services/VigenereCipher.java
+++ server/services/VigenereCipher.java
@@ -1,16 +1,8 @@
 package server.services;
 
 public class VigenereCipher {
-    private	String message;
-    private String key;
-    private String encrypted;
 
-    public VigenereCipher(String message, String key) {
-        this.message = message;
-        this.key = key;
-    }
-
-    public String encrypt() {
+    public static String encrypt(String message, String key) {
         StringBuilder result = new StringBuilder();
         key = key.toLowerCase();
         for (int i = 0, j = 0; i < message.length(); i++) {
@@ -26,12 +18,11 @@ public class VigenereCipher {
         return result.toString();
     }
 
-    public String decrypt() {
-        
+    public static String decrypt(String message, String key) {
         StringBuilder result = new StringBuilder();
         key = key.toLowerCase();
-        for (int i = 0, j = 0; i < encrypted.length(); i++) {
-            char ch = encrypted.charAt(i);
+        for (int i = 0, j = 0; i < message.length(); i++) {
+            char ch = message.charAt(i);
             if (Character.isLetter(ch)) {
                 char base = Character.isLowerCase(ch) ? 'a' : 'A';
                 int shift = key.charAt(j % key.length()) - 'a';
